ec4dad641364b4767c316d24ca3a138542a78cdf
Wishbebetter <whatasadstory@outlook.com>
2026-09-22 21:40:19 +0800

    katex_updated

--- scripts/katex.js
+++ scripts/katex.js
@@ -1,3 +1,9 @@
+const texmath = require('markdown-it-texmath')
+const katex = require('katex')
+
 hexo.extend.filter.register('markdown-it:renderer', function (md) {
-  md.use(require('markdown-it-katex'))
+  md.use(texmath, {
+    engine: katex,
+    delimiters: 'dollars'
+  })
 })
